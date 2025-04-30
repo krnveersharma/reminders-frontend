26b855347990a062ab8a28a280b4a9d9deeeb786
krnveersharma <karansharma10293847@gmail.com>
2025-05-01 02:04:49 +0530

    Added event streaming through server side events

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,38 @@
-import React from "react";
+"use client"
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const DashBoard=()=>{
+   const[remindersCount,setRemindersCount]=useState(0)
+   useEffect(()=>{
+      (async()=>{
+         try {
+            const res=await axios.get(`${process.env.SERVER}/dashboard/reminder-count`)
+            setRemindersCount(res?.data?.message)
+            
+            const eventSource=new EventSource(`${process.env.SERVER}/sse-dashboard/get-total-reminders`)
+
+            eventSource.onmessage=(event)=>{
+               const newCount=parseInt(event.data)
+               console.log("new count is:",newCount)
+               setRemindersCount(newCount)
+            }
+
+            eventSource.onerror=(error)=>{
+               console.log("SSE connection error:",error)
+               eventSource.close();
+            }
+            
+
+            return()=>{
+               eventSource.close();
+            }
+         } catch (error) {
+            
+         }
+      })()
+      
+   },[])
  return(
     <>
     </>
